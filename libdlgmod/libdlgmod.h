0ab0f4ff2d8016d1f2ea350206a37da6f20c9ee0
high on tantor <josephvenable@verizon.net>
2026-07-04 00:35:01 -0400

    Update libdlgmod.h

--- libdlgmod/libdlgmod.h
+++ libdlgmod/libdlgmod.h
@@ -24,6 +24,65 @@
 
 */
 
+#ifdef _WIN32 /* Windows */
+#define EXPORTED_FUNCTION extern "C" __declspec(dllexport)
+#else /* macOS, Linux, FreeBSD, DragonFly BSD, NetBSD, OpenBSD, Solaris, illumos */
+#define EXPORTED_FUNCTION extern "C" __attribute__((visibility("default")))
+#endif
+
+EXPORTED_FUNCTION double show_message(char *str);
+EXPORTED_FUNCTION double show_message_async(char *str);
+EXPORTED_FUNCTION double show_message_cancelable(char *str);
+EXPORTED_FUNCTION double show_message_cancelable_async(char *str);
+EXPORTED_FUNCTION double show_question(char *str);
+EXPORTED_FUNCTION double show_question_async(char *str);
+EXPORTED_FUNCTION double show_question_cancelable(char *str);
+EXPORTED_FUNCTION double show_question_cancelable_async(char *str);
+EXPORTED_FUNCTION double show_attempt(char *str);
+EXPORTED_FUNCTION double show_attempt_async(char *str);
+EXPORTED_FUNCTION double show_error(char *str, double abort);
+EXPORTED_FUNCTION double show_error_async(char *str, double abort);
+EXPORTED_FUNCTION char *get_string(char *str, char *def);
+EXPORTED_FUNCTION double get_string_async(char *str, char *def);
+EXPORTED_FUNCTION char *get_password(char *str, char *def);
+EXPORTED_FUNCTION double get_password_async(char *str, char *def);
+EXPORTED_FUNCTION double get_integer(char *str, double def);
+EXPORTED_FUNCTION double get_integer_async(char *str, double def);
+EXPORTED_FUNCTION double get_passcode(char *str, double def);
+EXPORTED_FUNCTION double get_passcode_async(char *str, double def);
+EXPORTED_FUNCTION char *get_open_filename(char *filter, char *fname);
+EXPORTED_FUNCTION double get_open_filename_async(char *filter, char *fname);
+EXPORTED_FUNCTION char *get_open_filename_ext(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION double get_open_filename_ext_async(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION char *get_open_filenames(char *filter, char *fname);
+EXPORTED_FUNCTION double get_open_filenames_async(char *filter, char *fname);
+EXPORTED_FUNCTION char *get_open_filenames_ext(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION double get_open_filenames_ext_async(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION char *get_save_filename(char *filter, char *fname);
+EXPORTED_FUNCTION double get_save_filename_async(char *filter, char *fname);
+EXPORTED_FUNCTION char *get_save_filename_ext(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION double get_save_filename_ext_async(char *filter, char *fname, char *dir, char *title);
+EXPORTED_FUNCTION char *get_directory(char *dname);
+EXPORTED_FUNCTION double get_directory_async(char *dname);
+EXPORTED_FUNCTION char *get_directory_alt(char *capt, char *root);
+EXPORTED_FUNCTION double get_directory_alt_async(char *capt, char *root);
+EXPORTED_FUNCTION double get_color(double defcol);
+EXPORTED_FUNCTION double get_color_async(double defcol);
+EXPORTED_FUNCTION double get_color_ext(double defcol, char *title);
+EXPORTED_FUNCTION double get_color_ext_async(double defcol, char *title);
+EXPORTED_FUNCTION char *widget_get_caption();
+EXPORTED_FUNCTION double widget_set_caption(char *str);
+EXPORTED_FUNCTION char *widget_get_owner();
+EXPORTED_FUNCTION double widget_set_owner(char *hwnd);
+EXPORTED_FUNCTION char *widget_get_icon();
+EXPORTED_FUNCTION double widget_set_icon(char *icon);
+EXPORTED_FUNCTION char *widget_get_system();
+EXPORTED_FUNCTION double widget_set_system(char *sys);
+EXPORTED_FUNCTION char *widget_get_button_name(double type);
+EXPORTED_FUNCTION double widget_set_button_name(double type, char *name);
+EXPORTED_FUNCTION double widget_get_canceled();
+EXPORTED_FUNCTION void RegisterCallbacks(char *arg1, char *arg2, char *arg3, char *arg4);
+
 namespace dialog_module {
 
   int show_message(char *str);
