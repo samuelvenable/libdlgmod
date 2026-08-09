b8321c9d0eac2693d9ee8c285f212b8d13079823
high on tantor <josephvenable@verizon.net>
2026-08-09 19:46:08 -0400

    Update libdlgmod.mm

--- libdlgmod/macos/libdlgmod.mm
+++ libdlgmod/macos/libdlgmod.mm
@@ -1252,9 +1252,9 @@ EOF
       std::vector<string> stringVec = string_split(strcol, ',');
       unsigned int index = 0;
       for (const string &str : stringVec) {
-        if (index == 0) r = (unsigned char)strtol(str.c_str(), nullptr, 10);
-        if (index == 1) g = (unsigned char)strtol(str.c_str(), nullptr, 10);
-        if (index == 2) b = (unsigned char)strtol(str.c_str(), nullptr, 10);
+        if (index == 0) r = strtod(str.c_str(), nullptr);
+        if (index == 1) g = strtod(str.c_str(), nullptr);
+        if (index == 2) b = strtod(str.c_str(), nullptr);
         index += 1;
       }
       return (int)(r | (g << 8) | (b << 16));
